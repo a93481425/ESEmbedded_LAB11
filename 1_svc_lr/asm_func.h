736200b1d39f7e64ad8da26a164901815d79d995
a93481425 <a93481425@gmail.com>
2019-06-05 18:58:52 +0800

    20190605

--- 1_svc_lr/asm_func.h
+++ 1_svc_lr/asm_func.h
@@ -11,5 +11,6 @@ uint32_t read_ctrl(void); //return the value of control
 void start_user(uint32_t *task_addr, uint32_t *psp_init); //start user task at task_addr in unprivileged mode and set sp to psp with value psp_init
 
 int32_t sys_call(void);
+int32_t sys_call_add(int a,int b);
 
 #endif
